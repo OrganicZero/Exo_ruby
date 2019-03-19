2db6045a34db725333e1fe87441536e502202fdf
Jules Dajon-Lamare <jules.dajonlamare@gmail.com>
2019-03-19 19:37:24 +0100

    ajout des exo

---
+++ exo_05.rb
@@ -0,0 +1,30 @@
+# affiche  "on va compter le nombre d'heures de travail à THP"
+puts "On va compter le nombre d'heures de travail à THP"
+# affiche "travail" et multiplie 10 * 5 *11
+puts "Travail : #{10 * 5 * 11}"
+# affiche "En minutes ça fait :" et multiplie l'opération contenu dans #{}
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# affiche "Et en secondes ?" 
+puts "Et en secondes ?"
+# Multiplie l'opération contenu dans #{}
+puts 10 * 5 * 11 * 60 * 60
+# Affiche "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Répond que 3 + 2 < 5 - 7 est faux
+puts 3 + 2 < 5 - 7
+# Affiche "Ca fait combien 3 + 2 ?" et affiche le résultat de l'opération contenu dans #{}
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# Affiche "ça fait combien 5 - 7 ?" et affiche le résultat de l'opération contenu dans #{}
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# Affiche " Ok, c'est faux alors !" 
+puts "Ok, c'est faux alors !"
+# Affiche "C'est drole ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :"
+# # Affiche la question en affiche directement le résultat opéré par ce qui se trouve entre  #{}
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# Affiche la question en affiche directement le résultat opéré par ce qui se trouve entre  #{}
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# Affiche la question en affiche directement le résultat opéré par ce qui se trouve entre  #{}
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#Le programme interpréte ce qu'il y a contenu dans les #{} et affiche directement le résultat à l'écran
+
